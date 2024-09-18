d92c570515babcb2126878f60e34277038943d89
83bc <mr.alkhazrji@gmail.com>
2024-09-19 02:54:58 +0300

    d

--- Burger-app/app/auth/index.tsx
+++ Burger-app/app/auth/index.tsx
@@ -2,13 +2,17 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
  import { Stack } from 'expo-router'
+  
+ 
 
+ 
 
 export default function index() {
   return (
   <>
   <Stack.Screen options={{headerShown:false}}/>
-   
+     
+ 
  
   </>
   )
