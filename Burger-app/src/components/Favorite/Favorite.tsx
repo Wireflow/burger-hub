c5fcc96a4e7153d2b747f9f0c7ad24913b794e95
Esam-alsermaah <alsrmaahesam@gmail.com>
2024-09-27 17:58:18 +0300

    fav

---
+++ Burger-app/src/components/Favorite/Favorite.tsx
@@ -0,0 +1,77 @@
+import { StyleSheet, View ,Text} from 'react-native';
+import React,{useState} from 'react';
+import CardWrapper from '../card/CardWrapper';
+import NotFound from '../notFound/NotFound';
+
+const Favorite = () => {
+    const [s,SetS]=useState(false)
+    const handlePress = () => {
+        console.log('Button Pressed');
+      };
+    if (s){
+        return (
+            <View style={styles.container}>
+            <Text style={styles.t}>Favorite</Text>
+              <View style={styles.row}>
+                <CardWrapper
+                  height={190}
+                  width={160}
+                  title='Burger'
+                  price='$99.90'
+                  imageSource={require('@/assets/images/dd.jpeg')}
+                />
+                <CardWrapper
+                  height={190}
+                  width={160}
+                  title='Burger'
+                  price='$99.90'
+                  imageSource={require('@/assets/images/dd.jpeg')}
+                />
+              </View>
+              <View style={styles.row}>
+                <CardWrapper
+                  height={190}
+                  width={160}
+                  title='Burger'
+                  price='$99.90'
+                  imageSource={require('@/assets/images/dd.jpeg')}
+                />
+                <CardWrapper
+                  height={190}
+                  width={160}
+                  title='Burger'
+                  price='$99.90'
+                  imageSource={require('@/assets/images/dd.jpeg')}
+                />
+              </View>
+            </View>
+          );
+    }else {
+        return <NotFound
+        
+        icon="heart-o" 
+        message1="No favorites"
+        message2="Choose your favorite items by hitting the heart"
+        buttonTitle="Start browsing"
+        onButtonPress={handlePress} 
+    
+        />
+    }
+
+};
+
+export default Favorite;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10, 
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    marginBottom: 10, 
+  },
+  t:{
+    fontSize:40
+  }
+});
