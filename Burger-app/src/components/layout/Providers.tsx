845d5bfc3ebe39fe135feb22cf2b3c21dfc2774b
83bc <mr.alkhazrji@gmail.com>
2024-09-27 22:28:09 +0300

    l

--- Burger-app/src/components/layout/Providers.tsx
+++ Burger-app/src/components/layout/Providers.tsx
@@ -1,4 +1,3 @@
-// src/providers/Providers.tsx
 import queryClient from "@/src/services/react-query";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,17 +6,29 @@ import {
     SafeAreaProvider,
     initialWindowMetrics,
 } from "react-native-safe-area-context";
- 
+import { ToastProvider } from "react-native-toast-notifications";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-  
+      <ToastProvider
+        placement="top"
+        offsetTop={50}
+        duration={3000}
+        animationType="slide-in"
+        swipeEnabled={false}
+        style={{
+          zIndex: 999999,
+          elevation: 999999, 
+        }}
+      >
         <ThemeProvider value={DefaultTheme}>
-             <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-              {children}
-            </SafeAreaProvider>
-         </ThemeProvider>
-     </QueryClientProvider>
+          <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+            {children}
+          </SafeAreaProvider>
+        </ThemeProvider>
+      </ToastProvider>
+    </QueryClientProvider>
   );
 };
 
