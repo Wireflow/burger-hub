c7740738024ce0d86c4852486a88fd7efe526898
Esam-alsermaah <alsrmaahesam@gmail.com>
2024-09-27 23:09:10 +0300

    end

--- Burger-app/src/components/layout/Providers.tsx
+++ Burger-app/src/components/layout/Providers.tsx
@@ -1,41 +1,19 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> bd14c0db002f360ff61e39ddc5de8b30e946afde
+// src/providers/Providers.tsx
 import queryClient from "@/src/services/react-query";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TabProvider } from "./TabContext";
 import React from "react";
 import {
     SafeAreaProvider,
     initialWindowMetrics,
 } from "react-native-safe-area-context";
-import { ToastProvider } from "react-native-toast-notifications";
-
+import { TabProvider } from "./TabContext";
+ 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ToastProvider
-        placement="top"
-        offsetTop={50}
-        duration={3000}
-        animationType="slide-in"
-        swipeEnabled={false}
-        style={{
-          zIndex: 999999,
-          elevation: 999999, 
-        }}
-      >
+  
         <ThemeProvider value={DefaultTheme}>
-<<<<<<< HEAD
-          <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-            {children}
-          </SafeAreaProvider>
-        </ThemeProvider>
-      </ToastProvider>
-    </QueryClientProvider>
-=======
              <SafeAreaProvider initialMetrics={initialWindowMetrics}>
               <TabProvider>
               {children}
@@ -43,7 +21,6 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
             </SafeAreaProvider>
          </ThemeProvider>
      </QueryClientProvider>
->>>>>>> bd14c0db002f360ff61e39ddc5de8b30e946afde
   );
 };
 
